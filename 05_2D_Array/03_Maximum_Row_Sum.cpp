e857a416f49b78bc3f4a065ad5d5ea3fedb5c443
PrachiSushir <prachisushir362@gmail.com>
2026-01-12 00:32:16 +0530

    Calculate the maximum sum of row of 2D matrix

---
+++ 05_2D_Array/03_Maximum_Row_Sum.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+ int maxRowSum(int mat[3][3], int row, int col){
+  int maxSum = INT_MIN;
+  for(int i=0; i<row; i++){
+    int rowSum = 0;
+    for(int j=0; j<col; j++){
+   rowSum += mat[i][j];
+    }
+    maxSum = max(maxSum, rowSum);
+  }
+  return maxSum;
+ }
+int main(){
+    int mat[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+    int row = 3;
+    int col = 3;
+    cout<<maxRowSum(mat,row,col);
+    return 0;
+}
